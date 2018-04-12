a95e42d021535016a4930164c64ac0083b1413f9
Scott Werwath <swerwath@berkeley.edu>
2018-04-11 22:18:33 -0700

    Added results link

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/results')
+def results():
+    lat = request.args.get('lat')
+    long = request.args.get('long')
+    return render_template("results.html", lat=lat, long=long)
