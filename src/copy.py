16a7d59257da94933cd3a37217c8ed4a06e4863c
Scott Werwath <swerwath@berkeley.edu>
2018-04-25 12:51:55 -0700

    Added xopy

--- src/copy.py
+++ src/copy.py
@@ -9,16 +9,16 @@ CHEM_NAMES = {
 
 HEALTH_RISKS = {
     "PM2.5" : "Long-term exposure increases the risk of death from <a href=\"https://www.ncbi.nlm.nih.gov/pubmed/20458016\">heart disease</a> and <a href=\"https://www.ncbi.nlm.nih.gov/pubmed/11879110\">lung cancer</a>. Prolonged exporsure can also lead to <a href=\"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3637008/\">quicker rate of artery thickening</a>, which increases the lifetime risk of developing cardiovascular disease.",
-    "SO2" : "Health risk sentence.",
-    "NO2" : "Health risk sentence.",
-    "OZONE" : "Health risk sentence.",
+    "SO2" : "Acute short-term exposure to SO<sub>2</sub> can cause breathing difficulty, especially for people with asthma or other vulnerable populations.",
+    "NO2" : "Long-term exposire to NO<sub>2</sub> is suspected to contribute to the development of asthma. Acute exposure can cause irration of the lungs and aggrevate existing respiratory diseases. NO<sub>2</sub> and other NO<sub>x</sub> compunds also lead to the development of acid rain, which can damage important ecosystems.",
+    "OZONE" : "Long-term exposure to ozone increases the chance of lung infection and can lead to the development of asthma, espcially in children. Acute short-term exposure can trigger asthma attacks or aggravate chronic bronchitis in people who already have those diseases. It can also cause difficulty of breathing and coughing, even in healthy people.",
 }
 
 INFO = {
     "PM2.5" : CHEM_NAMES["PM2.5"] + " refers to particulate matter that has a diameter of less than 2.5 micrometers. These fine particles can be emitted from a number of sources, including power plants, motor vehicles, and forest/residential fires. Because " + CHEM_NAMES["PM2.5"] + " particles are so small, they stay in the air for longer than their heavier counterparts, increasing the odds that a human breathes them in. Once they enter the body, the fine particles can penetrate into the lungs and circulatory system. " + HEALTH_RISKS["PM2.5"],
-    "SO2" : "GENERAL INFO",
-    "NO2" : "GENERAL INFO",
-    "OZONE" : "GENERAL INFO",
+    "SO2" : "Sulfer Dioxide (SO<sub>2</sub>) is an air pollutant released primarily from the burning of fossil fuels at power plants and large industrial facilities. In addtion to carrying its own health risks, SO<sub>2</sub> in the air can react with other chemicals in the environment to form PM<sub>2.5</sub> particulate matter (see above). " + HEALTH_RISKS["SO2"],
+    "NO2" : "Nitrogen Dioxide (NO<sub>2</sub>) is highly reactive gas emitted primarily from the burning of fuel, both from motor vehicles and power plants. NO<sub>2</sub> is a member of and an indicator for a group of chemicals called nitrogen oxides (NO<sub>x</sub>). " + HEALTH_RISKS["NO2"],
+    "OZONE" : "Ozone (O<sub>3</sub>) is a gas found both in the upper atmosphere and at ground level. Ground level ozone is not released directly into the air; rather, it is created as a product of chemical reactions between other air pollutants. These chemical reactions are accelerated on hot days, leading to increased ozone levels. " + HEALTH_RISKS["OZONE"],
 }
 
 EJ_EVIDENCE = {
@@ -32,24 +32,24 @@ LINK = {
     "PM2.5" : "#",
     "SO2" : "#",
     "NO2" : "#",
-    "OZONE" : "#",
+    "OZONE" : "https://www.epa.gov/ozone-pollution/health-effects-ozone-pollution",
 }
 
 DIST_VAR = {
-    "PM2.5" : "While PM<sub>2.5</sub> levels at various stations within a single city tend to be highly correlated, <a href=\"https://doi.org/10.1080/10473289.2004.10470919\">there is still a degree of variation</a> in measurements.",
+    "PM2.5" : "While PM<sub>2.5</sub> levels at various stations within a single city tend to be highly correlated, <a href=\"https://doi.org/10.1080/10473289.2004.10470919\">there is still some variation</a> in measurements.",
     "SO2" : "",
     "NO2" : "Since one of main sources of NO<sub>2</sub> is motor vehicles, <a href=\"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5348563/\">proximity to traffic</a> is a highly predictive of pollution levels. Since motor vehivle traffic varies over time (e.g. \"rush hour\") and air monitoring has low temporal resolution, it's important to keep in mind that NO<sub>2</sub> levels may higher than measured at certain times of day.",
     "OZONE" : "While ozone has <a href=\"https://www.sciencedirect.com/science/article/pii/0004698180900529\">relatively low spatial variation</a> in comparison to other pollutants, its distribution is still heavily dependent on nearby emission sources. If an ozone monitor is upwind of a large emission source, its reading may not be representative of the surrounding area.",
 }
 
 def dist_copy(dist, chem):
-    copy = "The nearest " + CHEM_NAMES[chem] + " monitoring station is " + ("%.1f" % dist) + " miles away from you. "
+    copy = "The nearest " + CHEM_NAMES[chem] + " monitoring station is " + ("%.1f" % dist) + " miles away from you"
     if dist < 6.21:
-        copy += "While all forms of air pollution have high variability, this monitor is likely close enough to give you an accurate estimate of the air pollution where you are."
+        copy += ", meaning that this monitor is likely close enough to give you an accurate estimate of the air pollution where you are."
     elif dist < 9.3:
-        copy += "While this station is not extremely close to you, it provides a moderately confident estimate of " + CHEM_NAMES[chem] + " levels near you. However, certain factors like wind and topological features may affect this accuracy."
+        copy += ". While this station is not very close to you, it provides a moderately confident estimate of " + CHEM_NAMES[chem] + " levels near you. However, certain factors like wind and topological features may affect this accuracy."
     else:
-        copy += "Given how far away the monitor is, it's unlikely that its measurements are representative of " + CHEM_NAMES[chem] + " levels near you."
+        copy += ". Given how far away the monitor is, it's unlikely that its measurements are representative of " + CHEM_NAMES[chem] + " levels near you."
 
     copy += " " + DIST_VAR[chem]
 
@@ -74,18 +74,19 @@ def aqi_copy(aqi, chem_name):
     return copy
 
 def get_copy(chem, nearest_monitors):
-    copy = INFO[chem] + "<br /><br />"
+    info = INFO[chem] + "<br /><br />"
     if not chem in nearest_monitors.keys():
-        copy += "It looks like <b>there are no " + CHEM_NAMES[chem] + " monitors in your area!</b> Why does this matter? " + \
+        dist = "It looks like <b>there are no " + CHEM_NAMES[chem] + " monitors in your area!</b> Why does this matter? " + \
         HEALTH_RISKS[chem] + " Since you don't live by any " + CHEM_NAMES[chem] + \
         " monitoring stations, there is no way for public health officials to estimate the exposure of people in your community. " + \
         EJ_EVIDENCE[chem] + " If you're concerned about the lack of " + CHEM_NAMES[chem] + " monitoring in your community, you can " + \
         "learn more about " + CHEM_NAMES[chem] + " <a href=\"" + LINK[chem] + "\" target=\"_blank\">here</a>, or keep scrolling to get involved " + \
         "with community air monitoring efforts and your local air district."
+        read = ""
     else:
-        dist = nearest_monitors[chem][1]
+        d = nearest_monitors[chem][1]
         aqi = nearest_monitors[chem][0]['AQI']
-        copy += dist_copy(dist, chem) + " <br /><br />"
-        copy += aqi_copy(aqi, CHEM_NAMES[chem])
+        dist = dist_copy(d, chem) + " <br /><br />"
+        read = aqi_copy(aqi, CHEM_NAMES[chem])
 
-    return Markup(copy)
+    return (Markup(info), Markup(dist), Markup(read))
